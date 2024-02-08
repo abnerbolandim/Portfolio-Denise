29d9b313ad9e312c2daf3a733116e32d5c3d5f60
abnerbolandim <contato.abnerbolandim@gmail.com>
2024-02-08 23:42:19 +0000

    update cards

--- src/components/Withdrawal.vue
+++ src/components/Withdrawal.vue
@@ -4,9 +4,11 @@
             <section>
                 <div class="card text-center">
                     <div class="card-body">
-                        <h5 class="card-title">Lorem ipsum dolor</h5>
-                        <p class="card-text">Lorem ipsum dolor sit</p>
-                        <a href="#" class="btn btn-primary" data-mdb-ripple-init>Faça o pedido</a>
+                        <h5 class="card-title">FAÇA SUA RETIRADA</h5>
+                        <p class="card-text">Pedidos feitos até 12h podem ser retirados até 19h do mesmo dia.<br>
+                                            Após este horário o prazo de retirar é até 1 dia útil até às 19h, após confirmação de pagamento.<br>
+                                            Os pedidos com retirada agendada devem ser feitos com 1 dia de antecedência.</p>
+                        <a href="#" class="btn" data-mdb-ripple-init>Contato</a>
                     </div>
                 </div>
             </section>
@@ -23,7 +25,37 @@ export default {
 </script>
 
 <style scoped>
-body{
+#app{
+    font-family: Arial, sans-serif;
+    justify-content: center;
+    padding: 25px;
+    background-color: #810947;
+}
+
+.card-title{
+    font-weight: 500;
+    padding-top: 20px;
+    font-size: 30px;
+    color: #810947;
+}
+
+.card-text{
+    font-weight: 600;
+    padding-top: 20px;
+    font-size: 20px;
+    color: #810947;
+}
+
+.btn{
+    color: white;
+    background-color: #810947;
+    shape-outside: none;
+    
+}
+.btn:hover{
+    color: rgb(222, 196, 229);
+    background-color: #811a4f;
+    shape-outside: none;
     
 }
 </style>
