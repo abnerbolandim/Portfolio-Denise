6e8ffd031e79e351490cf7b71002dcea6194893f
abnerbolandim <contato.abnerbolandim@gmail.com>
2024-02-09 14:28:51 +0000

    add new component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <Navbar/>
   <Withdrawal/>
+  <Categories/>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue'
 import Withdrawal from './components/Withdrawal.vue';
+import Categories from './components/Categories.vue';
 
 export default {
   name: 'App',
   components: {
     Navbar,
     Withdrawal,
+    Categories,
   }
 }
 </script>
