8d9f30f4d9532bed11e026fbb73941267eff0ca4
abnerbolandim <contato.abnerbolandim@gmail.com>
2024-04-24 23:13:28 +0100

    Add cards in Categories

--- src/components/Categories.vue
+++ src/components/Categories.vue
@@ -3,7 +3,33 @@
         <body>
             <section>
                 <div class="categories-title">
-                    <span >NOSSAS DELÍCIAS</span>
+                    <span >DESTAQUES</span>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 col-10">
+                        <div class="card" style="">
+                        <img src="../assets/IconDeniseBlack.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">Sabor Morango - 2€</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-10">
+                        <div class="card" style="">
+                        <img src="../assets/IconDeniseBlack.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">Caixa c/ Salgadinhos - 3€</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-10">
+                        <div class="card" style="">
+                        <img src="../assets/IconDeniseBlack.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">Sabor Paçoca - 2€</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </section>
         </body>
@@ -31,6 +57,23 @@ export default {
     color: #810947;
     font-size: 30px;
     font-weight: 600;
+}
+
+.card{
+    border-style: none;
+    margin: 20px;
+}
+
+.card img {
+	max-width: 100%;
+}
+
+.card-text {
+    text-align: center;
+	font-size: 17px;
+	margin-top: 15px;
+	margin-left: 10px;
+	margin-right: 10px;
 }
 
 </style>
