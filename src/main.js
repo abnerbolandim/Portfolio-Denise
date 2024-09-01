63a24313fed50945705da3a65da0cb3433118aac
Abner <contato.abnerbolandim@gmail.com>
2024-09-01 18:53:37 +0100

    Add leaflet

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'leaflet/dist/leaflet.css';
+
 
 import 'bootstrap/dist/css/bootstrap.css'
 import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
